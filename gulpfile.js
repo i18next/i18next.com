0af289d17e5804ad602365de65acfb3868a04d7d
Jan Muehlemann <jan.muehlemann@gmail.com>
2015-12-12 12:28:51 +0100

    fix build

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
 
-gulp.task('copyBundle', function() {
-   gulp.src('./public/*.{js,map}')
-   .pipe(gulp.dest('./public/i18next.com'));
-});
+// gulp.task('copyBundleIfNoDomain', function() {
+//    gulp.src('./public/*.{js,map}')
+//    .pipe(gulp.dest('./public/i18next.com'));
+// });
+//
+// gulp.task('copyLocalesIfNoDomain', function() {
+//    gulp.src('./pages/translate/**/locales/**/*.json')
+//    .pipe(gulp.dest('./public/i18next.com/translate/'));
+// });
+//
+// gulp.task('copyStaticIfNoDomain', function() {
+//    gulp.src('./static/**/*')
+//    .pipe(gulp.dest('./public/'));
+// });
+//
 
 gulp.task('copyLocales', function() {
    gulp.src('./pages/translate/**/locales/**/*.json')
-   .pipe(gulp.dest('./public/i18next.com/translate/'));
+   .pipe(gulp.dest('./public/translate/'));
 });
 
 gulp.task('copyStatic', function() {
@@ -16,4 +27,4 @@ gulp.task('copyStatic', function() {
 });
 
 
-gulp.task('copy', ['copyBundle', 'copyLocales', 'copyStatic']);
+gulp.task('copy', ['copyLocales', 'copyStatic']);
